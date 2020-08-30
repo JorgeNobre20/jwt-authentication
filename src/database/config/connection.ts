22391ed1fdd3bf709f39e4e63fb9335e54a57e2c
Jorge Nobre <jorgenobre1234@gmail.com>
2020-08-30 14:02:59 -0300

    start project and add database configuration

---
+++ src/database/config/connection.ts
@@ -0,0 +1,13 @@
+import knex from "knex";
+import path from "path";
+
+const db = knex({ 
+    client: "sqlite3", 
+    connection: path.resolve(__dirname, "..", "database.sqlite"),
+    migrations: {
+        directory: path.resolve(__dirname, "..", "migrations")
+    },
+    useNullAsDefault: true
+});
+
+export default db;
