7da366a136bb41e254d6f450e1c002e389dd0803
Jorge Nobre <jorgenobre1234@gmail.com>
2020-09-01 21:24:41 -0300

    add cors/update resources

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors";
+
 import Routes from "./routes";
 
 const app = express();
 
-
 app.use(express.json());
 app.use(Routes);
+app.use(cors());
 
 app.listen(3333, () => console.log("Server is running..."));
